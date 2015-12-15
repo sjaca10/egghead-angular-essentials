e7d17f328a2e6522a6f70764a5972b786da68d27
Javier Aguila <sjaca10@gmail.com>
2015-12-15 13:48:37 -0600

    Angular Essentials finished

--- eggly-app.start.js
+++ eggly-app.start.js
@@ -20,4 +20,112 @@ angular.module('Eggly', [
         {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor"},
         {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor"}
     ];
-})
+
+    $scope.currentCategory = null;
+
+    function setCurrentCategory(category) {
+        $scope.currentCategory = category;
+        cancelCreating();
+        cancelEditing();
+    }
+
+    function isCurrentCategory(category) {
+        return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
+    }
+
+    $scope.setCurrentCategory = setCurrentCategory;
+    $scope.isCurrentCategory = isCurrentCategory;
+
+    //-------------------------------------------------------------
+    // CRUD
+    //-------------------------------------------------------------
+    function resetCreateForm() {
+        $scope.newBookmark = {
+            title: '',
+            url: '',
+            category: $scope.currentCategory.name
+        }
+    }
+
+    function createBookmark(bookmark) {
+        bookmark.id = $scope.bookmarks.length;
+        $scope.bookmarks.push(bookmark);
+        console.log($scope.bookmarks)
+
+        resetCreateForm();
+    }
+
+    $scope.createBookmark = createBookmark;
+    $scope.editedBookmark = null;
+
+    function setEditedBookmark(bookmark) {
+        $scope.editedBookmark = angular.copy(bookmark);
+    }
+
+    function updateBookmark(bookmark) {
+        var index = _.findIndex($scope.bookmarks, function(b){
+            return b.id == bookmark.id;
+        });
+        $scope.bookmarks[index] = bookmark;
+
+        $scope.editedBookmark = null;
+        $scope.isEditing = false;
+    }
+
+    function isSelectedBookmark(bookmarkId) {
+        return $scope.editedBookmark !== null && $scope.editedBookmark.id === bookmarkId;
+    }
+
+    $scope.setEditedBookmark  = setEditedBookmark;
+    $scope.updateBookmark     = updateBookmark;
+    $scope.isSelectedBookmark = isSelectedBookmark;
+
+    function deleteBookmark(bookmark) {
+        _.remove($scope.bookmarks, function(b){
+            return b.id == bookmark.id;
+        });
+    }
+
+    $scope.deleteBookmark = deleteBookmark;
+
+    //-------------------------------------------------------------
+    // CREATING AND EDITING STATES
+    //-------------------------------------------------------------
+    $scope.isCreating = false;
+    $scope.isEditing = false;
+
+    function startCreating() {
+        $scope.isCreating = true;
+        $scope.isEditing = false;
+
+        resetCreateForm();
+    }
+
+    function cancelCreating() {
+        $scope.isCreating = false;
+    }
+
+    function startEditing() {
+        $scope.isCreating = false;
+        $scope.isEditing = true;
+    }
+
+    function cancelEditing() {
+        $scope.isEditing = false;
+    }
+
+    function shouldShowCreating() {
+        return $scope.currentCategory && !$scope.isEditing;
+    }
+
+    function shouldShowEditing() {
+        return $scope.isEditing && !$scope.isCreating;
+    }
+
+    $scope.startCreating      = startCreating;
+    $scope.cancelCreating     = cancelCreating;
+    $scope.startEditing       = startEditing;
+    $scope.cancelEditing      = cancelEditing;
+    $scope.shouldShowCreating = shouldShowCreating;
+    $scope.shouldShowEditing  = shouldShowEditing;
+});
